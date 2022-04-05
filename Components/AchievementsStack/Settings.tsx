9b9dd3bd0e1dc9a456f0561889859d6ae60c7f08
DillonKyleDev <dillonkyle95@gmail.com>
2022-04-04 18:07:57 -0700

    Changed icon images to work for app icon. Changed visual style for empty books read section. removed flex from settings/information tab

--- Components/AchievementsStack/Settings.tsx
+++ Components/AchievementsStack/Settings.tsx
@@ -23,7 +23,7 @@ const Settings: React.FC<Props> = ({navigation}) => {
     <View style={{flex: 1, backgroundColor: 'white'}}>
       <TopBar />
       <SectionHeader title="Settings / Information"/>
-      <View style={{flex: 1, display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}}>
+      <View style={{flex: 1}}>
         <View style={{margin: '8%'}}>
           <MyText text='This app was made for fun and for my own personal reading journey.  I hope it helps you too!' size={14} />
         </View>
@@ -39,7 +39,7 @@ const Settings: React.FC<Props> = ({navigation}) => {
           </>
           :
           <MyButton title='Reset Achievements' onPress={() => setShowWarning(true)} customStyle={{marginBottom: 0, marginTop: 0, width: 'auto', height: 'auto'}}/>}
-        <Text style={{color: 'blue', fontSize: 16, textDecorationLine: 'underline', marginLeft: 'auto', marginRight: 'auto', marginTop: 40}} onPress={() =>
+        <Text style={{color: 'blue', fontSize: 16, textDecorationLine: 'underline', marginLeft: 'auto', marginRight: 'auto', marginTop: 30}} onPress={() =>
           Linking.openURL("https://www.privacypolicies.com/live/0929da3a-3353-4024-8985-f24d43f2ab39")} accessibilityLabel="Privacy policy link">
             Privacy Policy
         </Text>
